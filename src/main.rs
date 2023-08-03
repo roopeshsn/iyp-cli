71fb9fa42fb1494f4d456a389e98d31ba981f5bc
Roopesh Saravanan <roopeshsaravanan.dev@gmail.com>
2023-08-03 23:04:06 +0530

    first commit

---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use neo4rs::*;
+use std::sync::Arc;
+
+#[tokio::main]
+async fn main() {
+    let uri = "iyp.iijlab.net:7687";
+    let user = "neo4j";
+    let pass = "neo";
+
+    let graph = Arc::new(Graph::new(uri, user, pass).await.unwrap());
+
+    let mut result = graph
+        .execute(
+            query("MATCH (a:AS {asn: $asn})-[:NAME]-(b:Name) RETURN a.asn as asn, b.name AS name")
+                .param("asn", 2497),
+        )
+        .await
+        .unwrap();
+
+    // Without using Some()
+    // let row = result.next().await.unwrap().unwrap();
+    // println!("{:?}", row.get::<String>("name").unwrap());
+
+    // Works for one item
+    // let get_result = result.next().await.unwrap();
+    // if let Some(row) = get_result {
+    //     if let Some(name) = row.get::<String>("name") {
+    //         println!("Name: {}", name);
+    //     } else {
+    //         println!("Name property not found in the result.");
+    //     }
+    // } else {
+    //     println!("No result returned from the query.");
+    // }
+
+    // while let Some(row) = result.next().await.unwrap() {
+    //     if let Some(name) = row.get::<String>("name") {
+    //         println!("Name: {}", name);
+    //     } else {
+    //         println!("Name property not found in the result.");
+    //     }
+    // }
+
+    while let Some(row) = result.next().await.unwrap() {
+        println!("{:?}", row);
+    }
+}
